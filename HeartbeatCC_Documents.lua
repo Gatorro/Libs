f4ff042a4e400594110b695f7776ddb7b4129d38
Gatorro <118382938+Gatorro@users.noreply.github.com>
2023-01-12 22:50:43 -0400

    Create HeartbeatCC_Documents.lua

---
+++ HeartbeatCC_Documents.lua
@@ -0,0 +1,34 @@
+-- \\\ << EXAMPLE 1, using OOP. >> /// --
+local HBLoop = Heartbeat:New(function(i)
+    print(i)
+end)
+HBLoop:Disconnect()
+-- \\\ << EXAMPLE 1, using OOP. >> /// --
+-- ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ --
+-- \\\ << EXAMPLE 2, normal functions. >> /// --
+Heartbeat:New(function(i)
+    print(i)
+end,'test')
+Heartbeat:Disconnect('test')
+-- \\\ << EXAMPLE 2, normal functions. >> /// --
+-- ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ --
+-- \\\ << EXAMPLE 3, Automatic token. >> /// --
+Heartbeat:New(function(i)
+    print(i)
+end)
+Heartbeat:Disconnect(1) -- Will stop we just created if there aren't more loops running, if there is 1 already running and u didn't set a custom name to it, the number should be 2.
+-- \\\ << EXAMPLE 3, Automatic token. >> /// --
+-- ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ --
+-- \\\ << SYNTAX >> /// --
+Heartbeat:New(f: function, token: any)
+--            ↑ Required    ↑ Optional
+Heartbeat:Disconnect(token: any)
+--                    ↑ Not required when using OOP.
+-- \\\ << SYNTAX >> /// --
+-- ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ --
+--[[
+\\\ << INFORMATION >> /// --
+   If the parameter token is not setted it will be setted to a INDEX depending on the already existing loops. (1 if there aren't loops.)
+   The callback, in this case "i": Will return a number that increases by 1 every time the loop repeats.
+\\\ << INFORMATION >> /// --
+]]
